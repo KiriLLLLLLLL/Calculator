1b57d5e5504c5586c62541a8e141868775d50519
BentecMike <kipsmv@gmail.com>
2022-02-28 23:09:18 +0500

    add code in files sum and dif

---
+++ sum.py
@@ -0,0 +1,13 @@
+x = 0 
+y = 0 
+ 
+# Модуль инициализации - ввода данных только внутри метода а не для всей программы без строк 6 и 7 
+def init(a, b): 
+    global x 
+    global y 
+    x = a 
+    y = b 
+ 
+def do_it(): 
+    return complex(x) + complex(y)
+
